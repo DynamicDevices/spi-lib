effb8054d5683d8fc5bb8791fa9ab2ce720bd319
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2025-03-25 14:11:41 +0000

    Updated register settings from Gowniyan
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- modules/lib/direct/default_mode_table.c
+++ modules/lib/direct/default_mode_table.c
@@ -30,53 +30,6 @@
 #include "direct.h"
 #include <string.h>
 
-/* Okan
-
-static const uint32_t presence[] =
-{
-    0x11e8270UL,
-    0x3088210UL,
-    0x9e967fdUL,
-    0xb0805b4UL,
-    0xd1027ffUL,
-    0xf010700UL,
-    0x11000000UL,
-    0x13000000UL,
-    0x15000000UL,
-    0x17000be0UL,
-    0x19000000UL,
-    0x1b000000UL,
-    0x1d000000UL,
-    0x1f000b60UL,
-    0x2113cc51UL,
-    0x236ff41fUL,
-    0x25006f7bUL,
-    0x2d000490UL,
-    0x3b000480UL,
-    0x49000480UL,
-    0x57000480UL,
-    0x5911be0eUL,
-    0x5b3ef40aUL,
-    0x5d00f000UL,
-    0x5f787e1eUL,
-    0x61f5208aUL,
-    0x630000a4UL,
-    0x65000252UL,
-    0x67000080UL,
-    0x69000000UL,
-    0x6b000000UL,
-    0x6d000000UL,
-    0x6f093910UL,
-    0x7f000100UL,
-    0x8f000100UL,
-    0x9f000100UL,
-    0xad000000UL,
-    0xb7000000UL,
-};
-*/
-
-// Gowniyan
-
 static const uint32_t presence[] =
 {
     0x11e8270UL,
@@ -117,6 +70,7 @@ static const uint32_t presence[] =
     0x9f000100UL,
     0xad000000UL,
     0xb7000000UL,
+    0xFFFFFFFF
 };
 
 static const uint32_t regs_landscape_460MHz[] =
@@ -160,6 +114,7 @@ static const uint32_t regs_landscape_460MHz[] =
     0x9F000100,
     0xAD000000,
     0xB7000000,
+    0xFFFFFFFF
 };
 
 static const uint32_t regs_landscape_1GHz[] =
@@ -264,7 +219,6 @@ static const uint32_t regs_utr11[] =
 #define XENSIV_BGT60TRXX_CONF_FRAME_REPETITION_TIME_S (0.00999593)
 #define XENSIV_BGT60TRXX_CONF_NUM_REGS (38)
 
-
 direct_mode_description_t
 direct_device_default_mode_table[] =
 {
@@ -275,8 +229,6 @@ direct_device_default_mode_table[] =
         {
             .num_samples_per_chirp = 128,
             .num_chirps_per_frame = 16,
-//            .bandwidth_Hz = (ifx_Float_t)460000000u,
-//            .center_frequency_Hz = (ifx_Float_t)61250000000u,
             .bandwidth_Hz = (ifx_Float_t)459804000u,
             .center_frequency_Hz = (ifx_Float_t)61250001000u,
             .orientation = IFX_ORIENTATION_LANDSCAPE,
